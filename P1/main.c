87813569740b74484b3f950e5fde017b1a3d4ed6
Zach Kaarvik <zkaarvik@gmail.com>
2015-02-16 15:52:02 -0800

    Example code

--- P1/main.c
+++ P1/main.c
@@ -1,98 +1,49 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdint.h>
+#include <time.h>
 
-#define MAX_LINE_LENGTH 256
-#define ID_CPUINFO "model name\t: "
-#define ID_MEMINFO "MemTotal:"
+unsigned long long timespecDiff(struct timespec *timeA_p, struct timespec *timeB_p)
+{
+  return ((timeA_p->tv_sec * 1000000000) + timeA_p->tv_nsec) -
+            ((timeB_p->tv_sec * 1000000000) + timeB_p->tv_nsec);
+}
 
-void print_cpuinfo();
-void print_kernel_version();
-void print_total_memory();
-void print_uptime();
 
 int main()
 {
-    print_cpuinfo();
-    print_kernel_version();
-	print_total_memory();
-	print_uptime();
+    struct timespec start;
+    struct timespec stop;
+    unsigned long long result; //64 bit integer
 
-    return 0;
-}
+    clock_gettime(CLOCK_REALTIME, &start);
+    sleep(1);
+    clock_gettime(CLOCK_REALTIME, &stop);
 
-/*
- *  Obtain processor type
- */
-void print_cpuinfo()
-{
-    FILE *fp_cpuinfo;
-    char str_input_line[MAX_LINE_LENGTH];
-    size_t index;
+    result=timespecDiff(&stop,&start);
 
-    fp_cpuinfo = fopen("/proc/cpuinfo", "r");
-    while (fgets(str_input_line, MAX_LINE_LENGTH, fp_cpuinfo))
-    {
-        //Check for line beginning with "model name", this is the processor type we need
-        if (strstr(str_input_line, ID_CPUINFO) != NULL)
-        {
-            index = strlen(ID_CPUINFO);
-            printf("Processor type:\t%s", str_input_line + index);
-            break;
-        }
-    }
-    fclose(fp_cpuinfo);
-}
+    printf("CLOCK_REALTIME Measured: %llu\n",result);
 
-/*
- *  Obtain kernel version
- */
-void print_kernel_version()
-{
-    FILE *fp_kernelversion;
-    char str_input_line[MAX_LINE_LENGTH];
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    sleep(1);
+    clock_gettime(CLOCK_MONOTONIC, &stop);
 
-    fp_kernelversion = fopen("/proc/sys/kernel/osrelease", "r");
-    //The only line in the file should be the kernel version
-    fgets(str_input_line, MAX_LINE_LENGTH, fp_kernelversion);
-    printf("Kernel version:\t%s", str_input_line);
-    fclose(fp_kernelversion);
-}
+    result=timespecDiff(&stop,&start);
 
-/*
- *  Obtain total memory
- */
-void print_total_memory()
-{
-	FILE *fp_meminfo;
-	char str_input_line[MAX_LINE_LENGTH];
-	size_t index;
+    printf("CLOCK_MONOTONIC Measured: %llu\n",result);
 
-    fp_meminfo = fopen("/proc/meminfo", "r");
-    while (fgets(str_input_line, MAX_LINE_LENGTH, fp_meminfo))
-    {
-        //Check for line beginning with "MemTotal:", it is followed by an unknown number of spaces before the memory amount
-        if (strstr(str_input_line, ID_MEMINFO) != NULL)
-        {
-            index = strlen(ID_MEMINFO);
-            while (str_input_line[index] == ' ') index++;
-            printf("Total memory:\t%s", str_input_line + index);
-            break;
-        }
-    }
-    fclose(fp_meminfo);
-}
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
+    sleep(1);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &stop);
 
-/*
- *  Obtain uptime
- */
-void print_uptime()
-{
-	FILE *fp_uptime;
-	char str_input_line[MAX_LINE_LENGTH];
+    result=timespecDiff(&stop,&start);
 
-	fp_uptime = fopen("/proc/uptime", "r");
-    //Use scanf to get the first characters in the file up to the first whitespace character, which is the uptime in seconds
-    fscanf(fp_uptime, "%s", str_input_line);
-    printf("System uptime:\t%s seconds\n", str_input_line);
-    fclose(fp_uptime);
+    printf("CLOCK_PROCESS_CPUTIME_ID Measured: %llu\n",result);
+
+    clock_gettime(CLOCK_THREAD_CPUTIME_ID, &start);
+    sleep(1);
+    clock_gettime(CLOCK_THREAD_CPUTIME_ID, &stop);
+
+    result=timespecDiff(&stop,&start);
+
+    printf("CLOCK_THREAD_CPUTIME_ID Measured: %llu\n",result);
 }
