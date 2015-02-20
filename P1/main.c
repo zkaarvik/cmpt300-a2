015a396bb153593f01daf62b82fbb884ef477f79
Zach Kaarvik <zkaarvik@gmail.com>
2015-02-20 14:52:36 -0800

    Context switching measurement implemented

--- P1/main.c
+++ P1/main.c
@@ -2,11 +2,14 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <time.h>
 #include <sched.h>
 #include <pthread.h>
 
-#define NUM_SAMPLES 10000000
+#define NUM_SAMPLES_LARGE 10000000
+#define NUM_SAMPLES_SMALL 1000000
 
 unsigned long long timespecDiff(struct timespec *timeA_p, struct timespec *timeB_p)
 {
@@ -38,14 +41,14 @@ double find_loop_overhead()
     int i;
 
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
-    for (i = 0; i < NUM_SAMPLES; i++)
+    for (i = 0; i < NUM_SAMPLES_LARGE; i++)
     {
 
     }
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &stop);
 
-    printf("Loop overhead: %f\n", (double) timespecDiff(&stop, &start) / NUM_SAMPLES);
-    return (double) timespecDiff(&stop, &start) / NUM_SAMPLES;
+    printf("Loop overhead: %f\n", (double) timespecDiff(&stop, &start) / NUM_SAMPLES_LARGE);
+    return (double) timespecDiff(&stop, &start) / NUM_SAMPLES_LARGE;
 }
 
 
@@ -56,15 +59,15 @@ void find_func_call_cost()
     int i;
 
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
-    for (i = 0; i < NUM_SAMPLES; i++)
+    for (i = 0; i < NUM_SAMPLES_LARGE; i++)
     {
         min_function();
     }
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &stop);
     total_time = timespecDiff(&stop, &start);
 
-    printf("Function call with loop ovh: %f\n", (double) total_time / NUM_SAMPLES);
-    printf("Measured function call cost: %f\n", ((double) total_time / NUM_SAMPLES) - find_loop_overhead());
+    printf("Function call with loop ovh: %f\n", (double) total_time / NUM_SAMPLES_LARGE);
+    printf("Measured function call cost: %f\n", ((double) total_time / NUM_SAMPLES_LARGE) - find_loop_overhead());
 }
 
 void find_sys_call_cost()
@@ -73,21 +76,94 @@ void find_sys_call_cost()
     unsigned long long total_time = 0;
     int i;
 
-    clock_gettime(CLOCK_MONOTONIC, &start);
-    for (i = 0; i < NUM_SAMPLES; i++)
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
+    for (i = 0; i < NUM_SAMPLES_LARGE; i++)
     {
         getpid();
     }
-    clock_gettime(CLOCK_MONOTONIC, &stop);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &stop);
     total_time = timespecDiff(&stop, &start);
 
-    printf("System call with loop ovh: %f\n", (double) total_time / NUM_SAMPLES);
-    printf("Measured systen call cost: %f\n", ((double) total_time / NUM_SAMPLES) - find_loop_overhead());
+    printf("System call with loop ovh: %f\n", (double) total_time / NUM_SAMPLES_LARGE);
+    printf("Measured system call cost: %f\n", ((double) total_time / NUM_SAMPLES_LARGE) - find_loop_overhead());
 }
 
+//////clock_gettime(CLOCK_MONOTONIC, &start);
 void find_proc_switch_cost()
 {
+    pid_t child_pid;
+    int pipefd1[2];
+    int pipefd2[2];
 
+    pipe(pipefd1);
+    pipe(pipefd2);
+
+    struct timespec start, stop;
+    unsigned long long total_time = 0;
+
+    //fork process to have a parent and child that will communicate
+    child_pid = fork();
+    if(child_pid == -1)
+    {
+        perror("Fork failure");
+        exit(1);
+    }
+
+    if(child_pid == 0)
+    {
+        //running in child
+        char readbuf;
+        char writebuf = '1';
+
+        //Close write side of first pipe, read side of second
+        close(pipefd1[1]);
+        close(pipefd2[0]);
+
+        //continually read from input pipe
+        //Then write back to output pipe
+        //Read from pipefd1[0]
+        while( read(pipefd1[0], &readbuf, sizeof(readbuf)) ){
+            //Write to pipefd2[1]
+            write(pipefd2[1], &writebuf, sizeof(writebuf));
+        }
+
+        close(pipefd1[0]);
+        close(pipefd2[1]);
+
+        exit(0);
+    } 
+    else
+    {
+        //running in parent
+        char readbuf;
+        char writebuf = '1';
+
+        //Close read side of first pipe, write side of second
+        close(pipefd1[0]);
+        close(pipefd2[1]);
+
+        clock_gettime(CLOCK_MONOTONIC, &start);
+
+        int j;
+        for(j = 0; j < NUM_SAMPLES_SMALL; j++)
+        {
+            //First write to pipe, then read
+            //Write to pipefd1[1]
+            write(pipefd1[1], &writebuf, sizeof(writebuf));
+
+            //Read from pipefd2[0]
+            read(pipefd2[0], &readbuf, sizeof(readbuf));
+        }
+
+        clock_gettime(CLOCK_MONOTONIC, &stop);
+        total_time = timespecDiff(&stop, &start);
+
+        close(pipefd1[1]);
+        close(pipefd2[0]);
+
+        //Double the number of samples as each counted for two context switches
+        printf("Measured context switch cost: %f\n", (double) total_time / (NUM_SAMPLES_SMALL * 2));
+    }
 }
 
 void find_thread_switch_cost()
