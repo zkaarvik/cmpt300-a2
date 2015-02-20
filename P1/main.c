7599da9a8e3ea22b4a923145c291baa74cb675e0
Zachary Kaarvik <zkaarvik@gmail.com>
2015-02-20 11:35:57 -0800

    Progress, CPU affinity is being set at application load

--- P1/main.c
+++ P1/main.c
@@ -1,6 +1,10 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdint.h>
 #include <time.h>
+#include <sched.h>
+#include <pthread.h>
 
 #define NUM_SAMPLES 10000000
 
@@ -10,26 +14,38 @@ unsigned long long timespecDiff(struct timespec *timeA_p, struct timespec *timeB
            ((timeB_p->tv_sec * 1000000000) + timeB_p->tv_nsec); //Calculate difference between two times, returning the difference in ns
 }
 
+void set_cpu_affinity()
+{
+    cpu_set_t mask;
+    CPU_ZERO(&mask);
+    CPU_SET(0, &mask);
+
+    if(sched_setaffinity(0, sizeof(mask), &mask) < 0)
+    {
+       perror("Set CPU Affinity Error");
+    }
+    if(pthread_setaffinity_np(pthread_self(), sizeof(mask), &mask) < 0)
+    {
+       perror("Set CPU Affinity Error");
+    }
+}
+
 void min_function() {}
 
-//FN to find gettime overhead???
-unsigned long long find_loop_overhead()
+double find_loop_overhead()
 {
-    struct timespec start, stop, dummy;
+    struct timespec start, stop;
+    int i;
 
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
-
-    int i;
     for (i = 0; i < NUM_SAMPLES; i++)
     {
-        //clock_gettime(CLOCK_MONOTONIC, &dummy);
-        //clock_gettime(CLOCK_MONOTONIC, &dummy);
-    }
 
+    }
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &stop);
 
-    printf("DEBUG: LOOP OVH: %f\n", (double) timespecDiff(&stop, &start) / NUM_SAMPLES);
-    return timespecDiff(&stop, &start) / NUM_SAMPLES;
+    printf("Loop overhead: %f\n", (double) timespecDiff(&stop, &start) / NUM_SAMPLES);
+    return (double) timespecDiff(&stop, &start) / NUM_SAMPLES;
 }
 
 
@@ -43,12 +59,11 @@ void find_func_call_cost()
     for (i = 0; i < NUM_SAMPLES; i++)
     {
         min_function();
-
     }
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &stop);
     total_time = timespecDiff(&stop, &start);
 
-    printf("DEBUG: W/O ACC FOR LOOP OVH: %f\n", (double) total_time / NUM_SAMPLES);
+    printf("Function call with loop ovh: %f\n", (double) total_time / NUM_SAMPLES);
     printf("Measured function call cost: %f\n", ((double) total_time / NUM_SAMPLES) - find_loop_overhead());
 }
 
@@ -66,8 +81,8 @@ void find_sys_call_cost()
     clock_gettime(CLOCK_MONOTONIC, &stop);
     total_time = timespecDiff(&stop, &start);
 
-    printf("DEBUG: W/O ACC FOR LOOP OVH: %f\n", (double) total_time / NUM_SAMPLES);
-    printf("Measured function call cost: %f\n", ((double) total_time / NUM_SAMPLES) - find_loop_overhead());
+    printf("System call with loop ovh: %f\n", (double) total_time / NUM_SAMPLES);
+    printf("Measured systen call cost: %f\n", ((double) total_time / NUM_SAMPLES) - find_loop_overhead());
 }
 
 void find_proc_switch_cost()
@@ -83,25 +98,22 @@ void find_thread_switch_cost()
 
 int main()
 {
-    struct timespec start;
-    struct timespec stop;
-    unsigned long long result;
     char user_choice;
+
+    set_cpu_affinity();
 
     while (1)
     {
-        printf("Choose what to measure: \n");
+        printf("\nChoose what to measure: \n");
         printf("1: Cost of a minimal function call\n");
         printf("2: Cost of a minimal system call\n");
         printf("3: Cost of process switching\n");
         printf("4: Cost of thread switching\n");
         printf("5: Exit\n\n");
         printf("Your choice?");
-        //user_choice = getchar();
-        //while(getchar() != '\n') //discard any other character on stdin until newline
-        //scanf(" %c", &user_choice);
+
         user_choice = fgetc(stdin);
-        while (user_choice != '\n' && fgetc(stdin) != '\n');
+        while (user_choice != '\n' && fgetc(stdin) != '\n'); //Clear buffer of extra chars
 
         switch (user_choice)
         {
@@ -118,9 +130,7 @@ int main()
             find_thread_switch_cost();
             break;
         case '5':
-            find_loop_overhead();
-            break;
-        //return 0;
+            return 0;
         default:
             printf("Invalid choice...\n");
         }
